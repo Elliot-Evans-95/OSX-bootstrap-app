ff62f2ec954a38c7326f74a1e6757604c6edf504
Elliot Evans <ell15evans.nuls@googlemail.com>
2020-12-15 21:06:56 +0000

    Initial Commit

---
+++ OSX-bootstrap-2/OSX_bootstrap_2App.swift
@@ -0,0 +1,17 @@
+//
+//  OSX_bootstrap_2App.swift
+//  OSX-bootstrap-2
+//
+//  Created by Elliot Evans on 15/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct OSX_bootstrap_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
